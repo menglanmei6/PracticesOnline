a630a4a5cedbacdd18cac1f2e62eec246427afb1
menglanmei6 <1229805664@qq.com>
2019-04-18 18:09:36 +0800

    2019年4月18日 实现业务逻辑

--- app/src/main/java/net/lzzy/practicesonline/activities/constants/DbConstants.java
+++ app/src/main/java/net/lzzy/practicesonline/activities/constants/DbConstants.java
@@ -8,12 +8,10 @@ import net.lzzy.sqllib.DbPackager;
  * @date 2019/3/11
  * Description:
  */
-public final class DbConstants {
+public class DbConstants {
     private DbConstants(){}
-    private static final String DB_NAME = "film.db";
+    private static final String DB_NAME = "practices.db";
     private static final int DB_VERSION = 1;
-    public static DbPackager packager;
-    static {
-        packager = DbPackager.getInstance(AppUtils.getContext(),DB_NAME,DB_VERSION, R.raw.models);
+    public static DbPackager packager=DbPackager
+            .getInstance(AppUtils.getContext(),DB_NAME,DB_VERSION, R.raw.models);
     }
-}
---
+++ app/src/main/java/net/lzzy/practicesonline/activities/models/Favorite.java
@@ -0,0 +1,29 @@
+package net.lzzy.practicesonline.activities.models;
+
+import net.lzzy.sqllib.Ignored;
+import net.lzzy.sqllib.Sqlitable;
+
+import java.util.UUID;
+
+/**
+ * Created by lzzy_gxy on 2019/4/16.
+ * Description:
+ */
+public class Favorite extends BaseEntity implements Sqlitable {
+    private UUID questionId;
+    @Ignored
+    public static final String COL_QUESTION_ID="colQuestionId";
+
+    public UUID getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(UUID questionId) {
+        this.questionId = questionId;
+    }
+
+    @Override
+    public boolean needUpdate() {
+        return false;
+    }
+}
